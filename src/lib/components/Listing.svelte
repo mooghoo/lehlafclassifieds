e414224b937ea818bd7dceb8ea959f7a10c2f5b3
mooghoo <albertopablito8@gmail.com>
2023-11-11 18:13:41 -0500

    fixed listing styles

--- src/lib/components/Listing.svelte
+++ src/lib/components/Listing.svelte
@@ -5,7 +5,7 @@
 	export let price = 0;
 </script>
 
-<div class="max-w-sm rounded overflow-hidden shadow-lg">
+<div class="mt-5 max-w-lg rounded overflow-hidden shadow-lg">
 	<img class="w-full" src={picture} alt={title} />
 	<div class="px-6 py-4">
 		<div class="font-bold text-xl mb-2">{title}</div>
