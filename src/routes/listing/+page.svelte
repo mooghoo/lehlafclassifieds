ddc3d4c7456ad6873cdead04d45678509dd04500
Noah Ross <me@noah.dev>
2023-11-11 19:07:00 -0500

    listing page start

---
+++ src/routes/listing/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Listing } from '$lib/listing';
+
+	export let data: Listing = {
+		id: 1,
+		title: 'Vintage Chair',
+		description: 'A classic wooden chair in good condition.',
+		picture:
+			'https://www.ikea.com/us/en/images/products/stefan-chair-brown-black__0727320_pe735593_s5.jpg?f=s',
+		price: 50,
+		phone: '555-555-5555',
+		school: 'University of Waterloo'
+	};
+</script>
