87ed91fc512f36220dd220d34b46bf717b5a62e5
Noah Ross <me@noah.dev>
2023-11-12 00:01:52 -0500

    fix broken comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,6 @@
 	import Listing from '$lib/components/Listing.svelte';
 </script>
 
-<!-- <Listing
-	title="Vintage Chair"
-	desc="A classic wooden chair in good condition."
-	picture="https://www.ikea.com/us/en/images/products/stefan-chair-brown-black__0727320_pe735593_s5.jpg?f=s"
-	price={50}
-	school="Lafayette College"
-/>
-
 {#each data.listings as item}
 	<Listing
 		title={item.title}
